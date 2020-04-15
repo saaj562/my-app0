4c7dbc0cefcabc72c870b38c909c358b1b930f14
saaj562 <57343087+saaj562@users.noreply.github.com>
2020-04-15 11:52:34 +0530

    Add files via upload

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,41 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+		public static void main(String args[]){
+			String input = "KovairSoftware"; 
+			  
+	        // getBytes() method to convert string  
+	        // into bytes[]. 
+	        byte [] strAsByteArray = input.getBytes(); 
+	  
+	        byte [] result =  
+	                   new byte [strAsByteArray.length]; 
+	  
+	        // Store result in reverse order into the 
+	        // result byte[] 
+	        for (int i = 0; i<strAsByteArray.length; i++) 
+	            result[i] =  
+	             strAsByteArray[strAsByteArray.length-i-1]; 
+	  
+	        System.out.println(new String(result)); 
+			
+			int a[]={1,2,3,2,11,15,14,16};
+			System.out.println("Odd Numbers:");
+			for(int i=0;i<a.length;i++){
+				if(a[i]%2!=0){
+					System.out.println(a[i]);
+				}
+			}
+			System.out.println("Even Numbers:");
+			for(int i=0;i<a.length;i++){
+				if(a[i]%2==0){
+					System.out.println(a[i]);
+				}
+			}
+		}
+	}
